aa7fce77df87a108726d4bdcfee5036f9cffbf69
Edwin Bwambale <precious@kali>
2024-08-15 22:12:42 +0300

    new file:   0-lockboxes.py         new file:   README.md

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+def canUnlockAll(boxes):
+    opened = [False] * len(boxes)
+    opened[0] = True
+    keys = boxes[0]
+
+
+    for key in keys:
+        if key < len(boxes) and not opened[key]:
+            opened[key] = True
+            keys.extend(boxes[key])
+
+    return all(opened)
+
+# Usage
+boxes = [[1], [2], [3], []]
+print(canUnlockAll(boxes))

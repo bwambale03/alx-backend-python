92c8873e85e56208d2caf9bd4a351fe34878a5ec
Edwin Bwambale <precious@kali>
2024-08-15 22:21:36 +0300

    updated the pycodestyle effect

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,13 +3,13 @@ def canUnlockAll(boxes):
     opened[0] = True
     keys = boxes[0]
 
-
     for key in keys:
         if key < len(boxes) and not opened[key]:
             opened[key] = True
             keys.extend(boxes[key])
 
     return all(opened)
+
 
 # Usage
 '''boxes = [[1], [2], [3], []]
